e5167242f0ac4325a87eb84cd8d8214ccbf15262
Laurent Kubler <lkubler@divalto.fr>
2018-08-10 15:19:10 +0200

    Update table schema

--- Domotique/Service/LogService.cs
+++ Domotique/Service/LogService.cs
@@ -8,18 +8,25 @@ namespace Domotique.Service.Log
 {
     public class LogService : ILogService
     {
+
         public void LogTemperatureService(String name, Double currentTemperature, Double? targetTemperature, DateTime logDate)
         {
             using (var connection = new MySqlConnection("server=192.168.1.34;port=3306;database=DomotiqueDev;uid=laurent;password=odile"))
             {
+                int RoomId = 0;
                 connection.Open();
                 using (var command = connection.CreateCommand())
                 {
-                    command.CommandText = "INSERT INTO `DomotiqueDev`.`TemperatureLog` (`CurrentTemp`,`LogDate`,`Room`,`TargetTemp`) VALUES" +
-                                    "(@CurrentTemp,@LogDate,@Room,@TargetTemp);";
+                    command.CommandText = "SELECT ID from Room Where Name = ?RoomName";
+                    RoomId = Int32.Parse(command.ExecuteScalar().ToString());
+                }
+                    using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO `DomotiqueDev`.`TemperatureLog` (`CurrentTemp`,`LogDate`,`RoomId`,`TargetTemp`) VALUES" +
+                                    "(@CurrentTemp,@LogDate,@RoomId,@TargetTemp);";
                     command.Parameters.AddWithValue("@CurrentTemp", currentTemperature);
                     command.Parameters.AddWithValue("@LogDate", logDate);
-                    command.Parameters.AddWithValue("@Room", name);
+                    command.Parameters.AddWithValue("@RoomId", RoomId);
                     command.Parameters.AddWithValue("@TargetTemp", targetTemperature);
                     command.ExecuteNonQuery();
                 }
