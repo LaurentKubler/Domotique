501fdebb06db636f0493b41850b20391cce111d9
Laurent Kubler <lkubler@divalto.fr>
2018-08-14 12:55:02 +0200

    Adding room status request

--- Domotique/Model/DataRead.cs
+++ Domotique/Model/DataRead.cs
@@ -1,7 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Messages.WebMessages;
 
 namespace Domotique.Model
 {
@@ -50,29 +50,83 @@ namespace Domotique.Model
             return newRoom;
         }
 
-         public void ReadRoomTemperatures()
+        public IList<RoomStatus> ReadRoomTemperatures()
         {
+            IList<RoomStatus> result = new List<RoomStatus>();
+
+            Dictionary<int, RoomStatus> rooms = new Dictionary<int, RoomStatus>();
             var connection = new MySqlConnection("server=192.168.1.34;port=3306;database=DomotiqueDev;uid=laurent;password=odile");
             connection.Open();
-            var command = connection.CreateCommand();
-            command.CommandText = "select  RoomId, Name, min(CurrentTemp) Min, max(CurrentTemp) Max,DATE(LogDate) " +
-                "from TemperatureLog " +
-                "inner join Room on Room.ID = TemperatureLog.RoomId " +
-                "where  LogDate >  date_sub(now(),INTERVAL 1 WEEK) group by  RoomId, DATE(LogDate);";
-            
-            
-            using (MySqlDataReader reader =  command.ExecuteReader())
+
+            using (var command = connection.CreateCommand())
             {
-                while (reader.Read()){
-                    var roomId = reader.GetInt32(0);
-                    var roomName = reader.GetString(1);
-                    var roomMin = reader.GetFloat(2);
-                    var roomMax = reader.GetFloat(3);
-                    var roomLastRefresh = reader.GetDateTime(4);
-                    Console.WriteLine($"{roomId}({roomName}) {roomMin}-{roomMax}")                    ;
+                command.CommandText = "Select * from Room;";
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    RoomStatus room = new RoomStatus()
+                    {
+                        RoomId = reader.GetInt32("ID"),
+                        RoomName = reader.GetString("Name"),
+                        Picture = reader.GetInt32("Picture")
+                    };
+                    rooms.Add(room.RoomId, room);
                 }
-            
+
             }
+       
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "select  RoomId, Name, min(CurrentTemp) Min, max(CurrentTemp) Max,DATE(LogDate) " +
+                    "from TemperatureLog " +
+                    "inner join Room on Room.ID = TemperatureLog.RoomId " +
+                    "where  LogDate >  date_sub(now(),INTERVAL 1 WEEK) group by  RoomId, DATE(LogDate);";
+
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var roomId = reader.GetInt32(0);
+                        var roomName = reader.GetString(1);
+                        var roomMin = reader.GetFloat(2);
+                        var roomMax = reader.GetFloat(3);
+                        var roomLastRefresh = reader.GetDateTime(4);
+                        rooms[roomId].Temperatures.Add(new DayTemperature()
+                        {
+                            MaxTemp = reader.GetFloat(3),
+                            MinTemp = reader.GetFloat(2),
+                            TemperatureDate = reader.GetDateTime(4)
+                        });
+                    }
+
+                }
+            }
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = 
+                    " select   " +
+                    "   l.RoomId, l.CurrentTemp, l.LogDate " +
+                    "from TemperatureLog l " +
+                    "where " +
+                    "   row(l.LogDate, l.roomId) in   ( select max(LogDate), RoomId from TemperatureLog group by RoomId); ";
+
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var roomId = reader.GetInt32(0);
+                        var roomLastRefresh = reader.GetDateTime(2);
+                        rooms[roomId].LastTemperatureRefresh = reader.GetDateTime(2);
+                        rooms[roomId].currentTemperature = reader.GetFloat(1);
+                    }
+
+                }
+            }
+
+            return result;
         }
     }
 }
---
+++ Messages/WebMessages/Status.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messages.WebMessages
+{
+    public class Status
+    {
+        public IList<RoomStatus> Rooms { get; set; }
+    }
+
+
+    public class RoomStatus
+    {
+        public int RoomId { get; set; }
+
+        public string RoomName { get; set; }
+
+        public int Picture { get; set; }
+
+        public IList<DayTemperature> Temperatures { get; set; }
+
+        public DateTime LastTemperatureRefresh { get; set; }
+
+        public float currentTemperature { get; set; }
+    }
+
+
+    public class DayTemperature
+    {
+        public DateTime TemperatureDate { get; set; }
+
+        public float MinTemp { get; set; }
+
+        public float MaxTemp { get; set; }
+
+    }
+}
