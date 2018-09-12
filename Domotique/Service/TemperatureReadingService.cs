a3a5ad90f92b70bdcb453dd90b1c478e8a940ce7
Laurent Kubler <lkubler@divalto.fr>
2018-09-12 11:52:25 +0200

    Added startup security

--- Domotique/Service/TemperatureReadingService.cs
+++ Domotique/Service/TemperatureReadingService.cs
@@ -5,6 +5,7 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
+using System.Threading;
 
 namespace Domotique.Service
 {
@@ -27,21 +28,33 @@ namespace Domotique.Service
 
         private IDataRead _dataRead;
 
-        public TemperatureReadingService  (IDataRead dataRead)
+        public TemperatureReadingService(IDataRead dataRead)
         {
             _dataRead = dataRead;
         }
-        public void  SetStatusService(IStatusService service)
+        public void SetStatusService(IStatusService service)
         {
-            statusService = service;            
+            statusService = service;
         }
 
 
         public void Start()
         {
-            
+
             var factory = new ConnectionFactory() { HostName = ServerName };
-            connection = factory.CreateConnection();
+            while (connection == null)
+            {
+                try
+                {
+                    connection = factory.CreateConnection();
+                }
+                catch (Exception e)
+                {
+                    Console.Write("unable to reach rabbitmq");
+                    Thread.Sleep(10000);
+                }
+                
+            }
             channel = connection.CreateModel();
             {
                 channel.QueueDeclare(queue: QueueName,
@@ -54,18 +67,18 @@ namespace Domotique.Service
                 consumer.Received += (model, ea) =>
                             {
                                 var body = ea.Body;
-                                
+
                                 var message = Encoding.UTF8.GetString(body);
                                 var temp = JsonConvert.DeserializeObject<ProbeTemperatureMessage>(message);
 
-                                String roomName = _dataRead.ReadRoomNameByProbe(temp.ProbeAddress);       
+                                String roomName = _dataRead.ReadRoomNameByProbe(temp.ProbeAddress);
 
-                                statusService.RegisterTemperature(roomName, temp.TemperatureValue,temp.MessageDate);
+                                statusService.RegisterTemperature(roomName, temp.TemperatureValue, temp.MessageDate);
                             };
                 channel.BasicConsume(queue: QueueName, autoAck: true, consumer: consumer);
             }
-            
-            IsStarted = true;            
+
+            IsStarted = true;
         }
 
         public void Dispose()
