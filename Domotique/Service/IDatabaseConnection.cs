f05d4d1b057d58c9bcf918c14a5739763077fa1b
Laurent Kubler <laurent.kubler@gmail.com>
2018-09-17 12:55:18 +0200

    Add interface

---
+++ Domotique/Service/IDatabaseConnection.cs
@@ -0,0 +1,12 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domotique.Service
+{
+    interface IDatabaseConnection
+    {
+        MySqlConnection GetConnection();
+    }
+}
