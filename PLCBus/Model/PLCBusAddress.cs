1e0797455a803075010469540f706ab21f0d993b
Laurent Kubler <laurent.kubler@gmail.com>
2018-09-18 11:49:56 +0200

    Add some classes/ tabs in home page

---
+++ PLCBus/Model/PLCBusAddress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PLCBus.Model
+{
+    public class PLCBusAddress
+    {
+        string HomeUnit { get; set; }
+
+        string UserCode { get; set; }
+    }
+}
