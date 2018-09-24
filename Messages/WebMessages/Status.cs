53f810aa29514751c9155a67d3ffea8ca1bddf6e
Laurent Kubler <laurent.kubler@gmail.com>
2018-09-24 17:56:04 +0200

    Fix index 0 access

--- Messages/WebMessages/Status.cs
+++ Messages/WebMessages/Status.cs
@@ -4,8 +4,8 @@ namespace Messages.WebMessages
 {
     public class Status
     {
-        public IList<RoomStatus> Rooms { get; set; }
+        public IList<RoomStatus> Rooms { get; set; } = new List<RoomStatus>();
 
-        public IList<DeviceStatus> Devices { get; set; }
+        public IList<DeviceStatus> Devices { get; set; } = new List<DeviceStatus>();
     }
 }
