d78cca5f0c1479c8bdb817b6342e2ae8852aba90
Laurent <laurent.kubler@gmail.com>
2018-10-11 22:17:58 +0200

    Add vl log/removed node log

--- ZWave_Python/ZWave_Python.py
+++ ZWave_Python/ZWave_Python.py
@@ -414,12 +414,12 @@ def callback(ch, method, properties, body):
     print("Activate switch {} on node {}".format(instance,nodeindex))
     try:
         for node in network.nodes:
-            print("node {}:{}:{}".format(node,network.nodes[node].node_id ,nodeindex))
             if (str(network.nodes[node].node_id) == str(nodeindex)):
                 print("node found {}".format(node))
                 for val in network.nodes[node].get_switches() :
-                    print("val  {}".format(val))
+                    print("val  {}:{}".format(network.nodes[node].values[val].index,instance))
                     if str(network.nodes[node].values[val].index) == str(instance):
+                        print("val found {}".format(node))
                         if Command == "PowerOn":
                             network.nodes[node].set_switch(val,True)
                         if Command == "PowerOff":
