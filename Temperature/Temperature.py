3bb30e7b8cb75e6232b3bfaf19a4908f59a6031f
Laurent <laurent.kubler@gmail.com>
2018-10-20 20:17:45 +0200

    Deploy graylog evolutions/Limit one wire logs

--- Temperature/Temperature.py
+++ Temperature/Temperature.py
@@ -23,7 +23,7 @@ def refresh():
         #channel.queue_declare(queue = cfg["Queue"]["queueName"])
         owproxy = pyownet.protocol.proxy(host = "192.168.1.42", port = 4304)
         #owproxy = pyownet.protocol.proxy(host="192.168.1.15", port=4304)
-        logger.info("beggining listing")
+        logger.info("beggining device listing")
         devices = owproxy.dir()
         for device in devices:
                 if device.find('/28') > -1:
@@ -35,9 +35,9 @@ def refresh():
                                 logger.info(datetime.datetime.now().isoformat() + ":Temp:" + dev['TemperatureValue'] + "@" + dev['ProbeAddress'])
                                 channel.basic_publish(exchange="InboundMessages",routing_key="device.temperature",body=json.dumps(dev))
                         except :
-                            logger.info("Oops!  That was no valid number.  Try again...")
+                            logger.debug("Oops!  That was no valid number.  Try again...")
                 else:
-                    logger.info("Device :" + device)
+                    logger.trace("Device type not recogniuzed :" + device)
         logger.info("Ending listing")
         connection.close()
 
@@ -46,10 +46,10 @@ logger = logging.getLogger()
 logger.setLevel(level=logging.INFO)
 logger.addHandler(GelfUdpHandler(host='graylog', _app_name='temperature', port=12201))
 
-logger.info("Starting ")
+logger.info("Starting temperature ")
 time.sleep(15)
 
-logger.info("First sleep finished")
+logger.trace("First sleep finished")
 refresh()
 scheduler = BlockingScheduler()
 scheduler.add_job(refresh, 'interval', seconds=cfg["Timeout"])
